11a8f374c437d4148f3402c8bc75941ba1c99816
Joshua Binotto <joshuabinotto2@gmail.com>
2024-11-15 00:54:31 -0300

    i'm sleepy so... good night

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './components/layout/main/main.component';
+import { DashboardComponent } from './components/layout/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'main/dashboard', pathMatch: 'full' },
+    { path: 'main', component: MainComponent, children: [
+        { path: 'dashboard', component: DashboardComponent }
+    ]}
+
+];
