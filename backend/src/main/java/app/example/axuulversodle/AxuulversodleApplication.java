c5c02f89d5eac4b8ac30306e65f8421c9eacf677
Joshua Binotto <joshuabinotto2@gmail.com>
2024-11-14 21:58:44 -0300

    fist commit

---
+++ backend/src/main/java/app/example/axuulversodle/AxuulversodleApplication.java
@@ -0,0 +1,13 @@
+package app.example.axuulversodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxuulversodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxuulversodleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/app/example/axuulversodle/AxuulversodleApplicationTests.java
@@ -0,0 +1,13 @@
+package app.example.axuulversodle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxuulversodleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
